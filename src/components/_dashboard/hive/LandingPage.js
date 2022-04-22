05845362cb362099dcc766853b5e14bcd9d18a97
LiFayi <lifayi2008@gmail.com>
2022-04-22 11:34:40 +0800

    code improvement

--- src/components/_dashboard/hive/LandingPage.js
+++ src/components/_dashboard/hive/LandingPage.js
@@ -218,6 +218,58 @@ export default function LandingPage() {
                 >
                   +
                 </Typography>
+
+                <Box
+                  sx={{
+                    transform: 'rotate(300deg)',
+                    position: 'absolute',
+                    top: '-23px',
+                    left: '-22px'
+                  }}
+                >
+                  <Box
+                    sx={{
+                      width: '16px',
+                      height: '16px',
+                      borderRadius: '8px',
+                      backgroundColor: '#FFC98F'
+                    }}
+                  />
+                  <Box
+                    sx={{
+                      width: '2px',
+                      height: '16px',
+                      backgroundColor: '#FFC98F',
+                      marginLeft: '7px'
+                    }}
+                  />
+                </Box>
+
+                <Box
+                  sx={{
+                    transform: 'rotate(120deg)',
+                    position: 'absolute',
+                    top: '21px',
+                    left: '53px'
+                  }}
+                >
+                  <Box
+                    sx={{
+                      width: '16px',
+                      height: '16px',
+                      borderRadius: '8px',
+                      backgroundColor: '#FFC98F'
+                    }}
+                  />
+                  <Box
+                    sx={{
+                      width: '2px',
+                      height: '16px',
+                      backgroundColor: '#FFC98F',
+                      marginLeft: '7px'
+                    }}
+                  />
+                </Box>
               </SmallHexagon>
             </Box>
             <CustomBox>1</CustomBox>
